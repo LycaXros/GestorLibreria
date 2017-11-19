17dadff4277dd4865fda188ca692b04edd2e4c29
Jesus Francisco Dicent Aguero <jesusdicent@gmail.com>
2017-11-19 17:43:45 -0400

    Datos
    Primer Commit

---
+++ GestorLibreria/Base.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace GestorLibreria
+{
+    partial class Base
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Base));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.UserNameLbl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnSalir = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menu1 = new System.Windows.Forms.MenuStrip();
+            this.mantenimientosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.librosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vagoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1.SuspendLayout();
+            this.menu1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnSalir,
+            this.UserNameLbl});
+            resources.ApplyResources(this.statusStrip1, "statusStrip1");
+            this.statusStrip1.Name = "statusStrip1";
+            // 
+            // UserNameLbl
+            // 
+            this.UserNameLbl.Name = "UserNameLbl";
+            resources.ApplyResources(this.UserNameLbl, "UserNameLbl");
+            this.UserNameLbl.Click += new System.EventHandler(this.UserNameLbl_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.IsLink = true;
+            this.btnSalir.Name = "btnSalir";
+            resources.ApplyResources(this.btnSalir, "btnSalir");
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // menu1
+            // 
+            this.menu1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mantenimientosToolStripMenuItem});
+            resources.ApplyResources(this.menu1, "menu1");
+            this.menu1.Name = "menu1";
+            // 
+            // mantenimientosToolStripMenuItem
+            // 
+            this.mantenimientosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.librosToolStripMenuItem,
+            this.usuariosToolStripMenuItem,
+            this.vagoToolStripMenuItem});
+            this.mantenimientosToolStripMenuItem.Name = "mantenimientosToolStripMenuItem";
+            resources.ApplyResources(this.mantenimientosToolStripMenuItem, "mantenimientosToolStripMenuItem");
+            // 
+            // librosToolStripMenuItem
+            // 
+            this.librosToolStripMenuItem.Name = "librosToolStripMenuItem";
+            resources.ApplyResources(this.librosToolStripMenuItem, "librosToolStripMenuItem");
+            // 
+            // usuariosToolStripMenuItem
+            // 
+            this.usuariosToolStripMenuItem.Name = "usuariosToolStripMenuItem";
+            resources.ApplyResources(this.usuariosToolStripMenuItem, "usuariosToolStripMenuItem");
+            // 
+            // vagoToolStripMenuItem
+            // 
+            this.vagoToolStripMenuItem.Name = "vagoToolStripMenuItem";
+            resources.ApplyResources(this.vagoToolStripMenuItem, "vagoToolStripMenuItem");
+            // 
+            // Base
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menu1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.HelpButton = true;
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menu1;
+            this.Name = "Base";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menu1.ResumeLayout(false);
+            this.menu1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel UserNameLbl;
+        private System.Windows.Forms.MenuStrip menu1;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem librosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vagoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel btnSalir;
+    }
+}
+
---
+++ GestorLibreria/Base.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestorLibreria
+{
+    public partial class Base : Form
+    {
+        public Base()
+        {
+            InitializeComponent();
+        }
+
+        private void UserNameLbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            
+            this.Close();
+        }
+    }
+}
